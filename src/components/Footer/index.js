a3955345f3fc84848db63f5701f788ce1f76e2f9
Mily <milyalvarez.26@gmail.com>
2020-11-07 00:58:50 -0300

    changes

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,22 +1,37 @@
-import React from 'react'
-import './style.scss'
+import React from "react";
+import "./style.scss";
 
 function Footer() {
-    return (
-        <div id="contact" className="contact">
-            
-            <h2>Contacto</h2>
-            <div className="myMail">
-                <div><img className="gmail" src="https://i.postimg.cc/y8JWWqQz/gmail.png" alt="Logo Gmail" /></div><p className="mail">milyalvarez.26@gmail.com</p>
-                
-            </div>
-            <div className="myLinkedin">
-                <div><img className="linkedin" src="https://i.postimg.cc/8zRSM0GN/linkedin.png" alt="Logo Linkedin" /></div><a href="https://www.linkedin.com/in/milagros-%C3%A1lvarez-cisterna-9891501b0/">Linkedin</a>
-            </div>
-
-            
+  return (
+    <div id="contact" className="contact">
+      <h2>Contacto</h2>
+      <div className="myContact">
+        <div className="myMail circle">
+          <a href="mailto:milyalvarez.26@gmail.com">
+            <img
+              className="gmail"
+              src="https://i.postimg.cc/y8JWWqQz/gmail.png"
+              alt="Logo Gmail"
+            />
+          </a>
         </div>
-    )
+        <div className="myLinkedin circle">
+          <a href="https://www.linkedin.com/in/milagros-%C3%A1lvarez-cisterna-9891501b0/">
+            <img
+              className="linkedin"
+              src="https://i.postimg.cc/8zRSM0GN/linkedin.png"
+              alt="Logo Linkedin"
+            />
+          </a>
+        </div>
+        <div className="myGithub circle">
+            <a href="https://github.com/Mily26">
+                <img className="github" src="./images/github-logo.png" alt="logo github"/>
+            </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
